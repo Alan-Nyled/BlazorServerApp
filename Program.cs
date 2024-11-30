90c1c7bfa16e8fb886d1b7002a442e989731ef7b
alannyled <84908245+alannyled@users.noreply.github.com>
2024-11-30 22:08:52 +0100

    ændring i program.cs for at imødekomme Google Cloud og viderestilling fra eget domæne

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace BlazorServerApp
 {
@@ -16,7 +17,8 @@ namespace BlazorServerApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -42,6 +44,13 @@ namespace BlazorServerApp
                 app.UseHsts();
             }
 
+            // Middleware to handle forwarded headers from a proxy (such as Google Cloud Run).
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            // Use HTTPS redirection (important for handling secure connections correctly)
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
